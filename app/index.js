2c0f814742ee33c2d984b534465090536b01d548
Joe <joseph.vinten@gmail.com>
2021-04-28 14:22:14 +0100

    inital commit

---
+++ app/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Popular from "./components/Popular";
+import "./index.css";
+
+// Component
+// State
+// Lifecycle
+// UI
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <Popular />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
