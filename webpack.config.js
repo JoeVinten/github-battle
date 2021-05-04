5c0243e65d45693688826cedeceea864a8f0ab09
Joe Vinten <joseph.vinten@gmail.com>
2021-05-04 13:51:20 +0100

    fix copyplugin import

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWeback = require("copy-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./app/index.js",
